fe9cf1f93def90930dc32d81fceef8d9c24db538
FuryFight3r <44615800+FuryFight3r@users.noreply.github.com>
2020-07-17 14:20:56 +1000

    Fixed my boo boo
    my bad

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,6 +8,22 @@ description 'DGRP McDonalds Job'
 
 version '2.6'
 
+this_is_a_map 'yes'
+
+files {
+    'stream/CarData/vehicles.meta',
+    'stream/CarData/carvariations.meta',
+    'stream/CarData/handling.meta',
+    'stream/CarData/dlctext.meta',
+    'stream/CarData/carcols.meta'
+}
+
+data_file 'CARCOL_FILE' 'stream/CarData/carcol.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/CarData/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'stream/CarData/dlctext.meta'
+data_file 'HANDLING_FILE' 'stream/CarData/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'stream/CarData/vehicles.meta'
+
 server_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
